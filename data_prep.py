adcc734fc8a36afcb2db7986f80c2415d5d4939a
Piotr Delikat <piotrdellikat@gmail.com>
2017-09-28 17:47:42 +0200

    Requested/Delivered amount to integer, columns dropped

--- data_prep.py
+++ data_prep.py
@@ -1,16 +1,20 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('Subsidy_Register.csv')
-
-print(df.head())
+df = pd.read_csv('Subsidy_Register.csv', na_values='€ NaN')
+df.drop(['Established', 'Year'], axis=1, inplace=True)
 
 #Cleaning
+
 #Eur to integers
+to_int = ['Requested', 'Delivered']
+for column in to_int:
+    df[column].fillna(0, inplace=True)
+    df[column] = df[column].replace('[\€|\.]', '', regex=True).astype(float)
 
+print(df.info())
 
-
-
-#Features: list Name [0|1], Project words (is words tokenization nesscessary?), Project lenght,
+#Features: list Name [0|1], Project words (is words tokenization nesscessary?), Project description length,
 # Arrangement (subsidy program), Organization (district), Periodicity (0|1), Requested
 
 #Count the labels from delivered = % of requested
