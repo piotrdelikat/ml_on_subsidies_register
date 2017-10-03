d1daf879807503c20f64e7815233e0b0b54bded6
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-03 13:09:10 +0200

    Organization, Theme, Periodicity to numerical

--- data_prep.py
+++ data_prep.py
@@ -4,6 +4,7 @@ import numpy as np
 df = pd.read_csv('Subsidy_Register.csv', na_values='€ NaN')
 df.drop(['Established', 'Year'], axis=1, inplace=True)
 
+
 #Cleaning
 
 #Eur to integers
@@ -12,9 +13,28 @@ for column in to_int:
     df[column].fillna(0, inplace=True)
     df[column] = df[column].replace('[\€|\.]', '', regex=True).astype(float)
 
-print(df.info())
 
-#Features: list Name [0|1], Project words (is words tokenization nesscessary?), Project description length,
+def handle_non_numerical_data(df):
+    to_numeric = ['Organization', 'Theme', 'Periodicity']
+    for column in to_numeric:
+        text_digit_vals = {}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        column_contents = df[column].values.tolist()
+        unique_elements = set(column_contents)
+        x = 0
+        for unique in unique_elements:
+            if unique not in text_digit_vals:
+                text_digit_vals[unique] = x
+                x+=1
+        df[column] = list(map(convert_to_int, df[column]))
+    return df
+
+df = handle_non_numerical_data(df)
+print(df.head())
+
+#Features: list Name [0|1], Project words to num, Project description length = new column,
 # Arrangement (subsidy program), Organization (district), Periodicity (0|1), Requested
 
 #Count the labels from delivered = % of requested
