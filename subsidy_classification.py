8e4ad1555983d7579d1d1aa15146f21a817c14a1
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-13 23:44:54 +0200

    working model, input

--- subsidy_classification.py
+++ subsidy_classification.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
@@ -11,15 +12,10 @@ df = pd.read_csv('cleaned.csv')
 X = df['Project'].values
 y = df['subsidy'].values
 
-
 count_vect = CountVectorizer()
 tfidf_transformer = TfidfTransformer()
 
-X_counts = count_vect.fit_transform(X)
-X_tfidf = tfidf_transformer.fit_transform(X_counts)
-
-
-X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size = 0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
 
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer()),
@@ -31,4 +27,8 @@ text_clf = text_clf.fit(X_train, y_train)
 predicted = text_clf.predict(X_test)
 print(np.mean(predicted == y_test))
 
+predict = sys.argv[1]
+predicted_subsidy = text_clf.predict(predict)
+print(predicted_subsidy)
 
+#changeing number to subsidy name
