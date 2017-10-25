5d64ced8f566f9eaddcf7057817b7ef8f863c7a8
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-25 23:51:23 +0200

    Basic model for storing data

--- classifier/descriptions/models.py
+++ classifier/descriptions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Prediction(models.Model):
+    description = models.CharField(max_length=300)
+    subsidy_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.subsidy_name
